e1195076d5b61bf699e93e35e0985190e92ca507
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-30 05:36:09 -0400

    Se creó el diseño de la ventana de login

--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -17,7 +17,7 @@ const Stack = createStackNavigator<RootStackParams>();
 export const StackNavigator = () => {
   return (
     <Stack.Navigator
-    initialRouteName="HomeScreen"
+    initialRouteName="LoginScreen"
     screenOptions={{
       headerShown: false,
     }}>
