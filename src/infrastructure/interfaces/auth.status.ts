865b47bb4e3a75b9dd67302ae6c03b374e470bfc
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-30 06:43:55 -0400

    Se instaló y configuró zustand y se creó el storage de auth y la función login

---
+++ src/infrastructure/interfaces/auth.status.ts
@@ -0,0 +1,2 @@
+
+export type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
---
+++ src/presentation/store/auth/useAuthStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { User } from '../../../domain/entities/user';
+import { AuthStatus } from '../../../infrastructure/interfaces/auth.status';
+import { authLogin } from '../../../actions/auth/auth';
+
+export interface AuthState {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+
+  login: (email: string, password: string) => Promise<boolean>;
+}
+
+export const useAuthStore = create<AuthState>()( (set, get) => ({
+  status: 'checking',
+  token: undefined,
+  user: undefined,
+
+  login: async (email: string, password: string) => {
+    const resp = await authLogin(email, password);
+    if ( !resp ) {
+      set({ status: 'unauthenticated', token: undefined, user: undefined });
+      return false;
+    }
+
+    // TODO: Guardar el token en el storage
+
+    set({ status: 'authenticated', token: resp.token, user: resp.user });
+
+    return true;
+  },
+
+}))
+
