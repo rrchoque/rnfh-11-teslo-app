505146898e350fbd03815a83db09b0534dd021bf
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-31 08:51:53 -0400

    Se creó la funcionalidad de logout

--- src/presentation/store/auth/useAuthStore.ts
+++ src/presentation/store/auth/useAuthStore.ts
@@ -11,6 +11,7 @@ export interface AuthState {
 
   login: (email: string, password: string) => Promise<boolean>;
   checkStatus: () => Promise<void>;
+  logout: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>()( (set, get) => ({
@@ -44,7 +45,10 @@ export const useAuthStore = create<AuthState>()( (set, get) => ({
     set({ status: 'authenticated', token: resp.token, user: resp.user });
   },
 
-
+  logout: async () => {
+    await StorageAdapter.removeItem('token');
+    set({ status: 'unauthenticated', token: undefined, user: undefined });
+  }
 
 }))
 
