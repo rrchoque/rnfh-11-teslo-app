0ca360d9703b6696637b9a555297578a0a3eaffa
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-01 07:31:50 -0400

    Se agregó infinit scroll de los productos

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -3,12 +3,25 @@ import { Layout, List, Text } from '@ui-kitten/components';
 
 import {Product} from '../../../domain/entities/product';
 import { ProductCard } from './ProductCard';
+import { useState } from 'react';
+import { RefreshControl } from 'react-native';
 
 interface Props {
   products: Product[];
+  fetchNextPage: () => void;
 }
 
-export const ProductList = ({products}: Props) => {
+export const ProductList = ({products, fetchNextPage}: Props) => {
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+
+  const onPullToRefresh = async() => {
+    setIsRefreshing(true);
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    setIsRefreshing(false);
+  }
 
   return (
     <List
@@ -17,6 +30,16 @@ export const ProductList = ({products}: Props) => {
       keyExtractor={(item, index) => `${item.id}-${index}`}
       renderItem={({item}) => <ProductCard product={ item } />}
       ListFooterComponent={() => <Layout style={{height: 150}} />}
+
+      onEndReached={ fetchNextPage }
+      onEndReachedThreshold={0.8}
+
+      refreshControl={ 
+        <RefreshControl
+          refreshing={ isRefreshing }
+          onRefresh={ onPullToRefresh }
+        />
+      }
     />
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { Button, Icon, Layout, Text } from "@ui-kitten/components"
 import { useAuthStore } from "../../store/auth/useAuthStore";
 import { getProductsByPage } from "../../../actions/products/get-products-by-page";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { MainLayout } from "../../layouts/MainLayout";
 import { FullScreenLoader } from "../../components/ui/FullScreenLoader";
 import { ProductList } from "../../components/products/ProductList";
@@ -12,12 +12,20 @@ export const HomeScreen = () => {
   const { logout } = useAuthStore();
 
   // Queries
-  const {isLoading, data: products = []} = useQuery({ 
+  // const {isLoading, data: products = []} = useQuery({ 
+  //   queryKey: ['products', 'infinite'],
+  //   staleTime: 1000 * 60 * 60, // 1 hour
+  //   queryFn: async params => await getProductsByPage(0),
+  // })
+  
+  const {isLoading, data, fetchNextPage} = useInfiniteQuery({
     queryKey: ['products', 'infinite'],
     staleTime: 1000 * 60 * 60, // 1 hour
-    queryFn: async params => await getProductsByPage(0),
-  })
-  
+    initialPageParam: 0,
+
+    queryFn: async params => await getProductsByPage(params.pageParam),
+    getNextPageParam: (lastPage, allPages) => allPages.length,
+  });
 
   return (
     <MainLayout
@@ -26,7 +34,10 @@ export const HomeScreen = () => {
     {isLoading ? (
       <FullScreenLoader />
     ) : (
-      <ProductList products={products} />
+      <ProductList
+        products={data?.pages.flat() ?? []}
+        fetchNextPage={fetchNextPage}
+      />
     )}
   </MainLayout>
 
